06bf0681437249e3d60761681d44bf2627b22249
erickwer <erickrodriguesmusic@hotmail.com>
2019-02-12 22:50:20 -0300

    Atualizando comentarios

--- Exercicio 10 -  Matemática Discreta.py
+++ Exercicio 10 -  Matemática Discreta.py
@@ -1,12 +1,13 @@
 
-# Pertinencia
+# Pertinencia (funciona com lista)
 a = {1, 2, 3, 4}
 print('O numero 9 pertence  a A?',9 in a)
 print('O numero 1 pertence  a A?',1 in a)
 
 
 
-# Continencia
+# Continencia (intersection não serve pra lista)
+
 a = {1, 2, 3, 4}
 b = {1, 2, 3, 4, 5, 6, 7}
 c = a.intersection(b)
@@ -15,13 +16,13 @@ if a == c:
 else:
     print("O conjunto A não está contido em B")
 
-# Subconjunto
+# Subconjunto (issubset não serve pra lista)
 a = {1, 2, 3, 4, 5}
 c = {1, 2, 3, 4}
 print('O conjunto C é Subconjunto de A? ', c.issubset(a))
 
 
-# Igualdade
+# Igualdade (na lista a ordem dos itens importa)
 a = {4, 2, 3, 1, 8}
 c = {4, 3, 1, 2, 8}
 if a == c:
